adbc543ae69449dfdf6142b0e3c1b4b4db73f2a6
JudeJeanPierre <110211488+JudeJeanPierre@users.noreply.github.com>
2022-11-09 06:57:05 -0600

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">HAITIPROD MART</a>
+      </header>
+      <main>
+        Main Products 
+      </main>
+    </div>
+  );
+}
+
+export default App;
